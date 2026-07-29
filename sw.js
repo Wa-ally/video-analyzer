276635915d22baea213ed14e27329ca3bbc944e8
Wa-ally <1214405543@qq.com>
2026-07-29 10:15:31 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'video-analyzer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/data.js',
+  './js/analyzer.js',
+  './js/rewriter.js',
+  './js/app.js',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && e.request.url.startsWith(self.location.origin)) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
